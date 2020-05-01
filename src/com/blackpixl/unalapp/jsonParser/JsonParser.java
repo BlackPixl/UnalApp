7beb50242db7a9797bda537e895b76fed49941e9
BlackPixl <javierdarioechavarria@gmail.com>
2020-05-01 16:06:27 -0500

    Added Read File Functionality.

--- src/com/blackpixl/unalapp/jsonParser/JsonParser.java
+++ src/com/blackpixl/unalapp/jsonParser/JsonParser.java
@@ -1,5 +1,26 @@
 package com.blackpixl.unalapp.jsonParser;
 
+import com.blackpixl.unalapp.user.User;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class JsonParser{
+
+    public static User load(){
+        String raw = "";
+        try {
+            raw = readFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Gson gson = new Gson();
+        return gson.fromJson(raw, User.class);
+    }
+
+    private static String readFile() throws IOException {
+        return Files.readString(Paths.get(".","User.json"));
+    }
 
 }
--- src/com/blackpixl/unalapp/main/Main.java
+++ src/com/blackpixl/unalapp/main/Main.java
@@ -2,9 +2,6 @@ package com.blackpixl.unalapp.main;
 
 import com.blackpixl.unalapp.jsonParser.JsonParser;
 import com.blackpixl.unalapp.user.User;
-import org.jetbrains.annotations.NotNull;
-
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -20,12 +17,7 @@ public class Main {
         switch(option){
             case 1:login();
                 break;
-            case 2:/*try{
-                loadState(input);
-                }catch(IOException e){
-                System.out.println(e.getMessage());
-                }*/
-                break;
+            case 2: loadState();
             case 3:queryCitation();
                 break;
             case 4:querySchedule();
@@ -36,18 +28,16 @@ public class Main {
     public static void login(){
         System.out.println("Funcionalidad bajo construcción");
     }
-/*
-    public static void loadState(@NotNull Scanner input)throws IOException{
-        System.out.println("Ingresa el nombre del archivo: ");
-        String filename = input.next();
-        User user = JsonParser.load(filename);
+
+    public static void loadState(){
+        User user = JsonParser.load();
         System.out.println(user.getName());
         System.out.println(user.getAcademicHistory().getPAPA());
         user.getAcademicHistory().recalculatePapa();
         System.out.println(user.getAcademicHistory().getPAPA());
         System.out.printf("%.1f", user.getAcademicHistory().getPAPA());
         user.getAcademicHistory().printCourses();
-    }*/
+    }
 
     public static void queryCitation(){
         System.out.println("Funcionalidad bajo construcción");
